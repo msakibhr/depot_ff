58b6bbe455feaf0bafb1e08c5e9fc9ec7ddd5097
msakibhr <msakibhr@gmail.com>
2023-05-08 19:40:11 +0600

    Iteration G2 done

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,6 @@
 #---
 Rails.application.routes.draw do
   resources :orders
-  # resources :line_items
-
 
   resources :line_items do
     member do
@@ -17,10 +15,12 @@ Rails.application.routes.draw do
     end
   end
 
-
   resources :carts
   root 'store#index', as: 'store_index'
-  resources :products
-  # For details on the DSL available within this file, see
+  resources :products do
+  get :who_bought, on: :member
+end
+
+# For details on the DSL available within this file, see
   # https://guides.rubyonrails.org/routing.html
 end
