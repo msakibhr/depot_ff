2cb67213219218582a4da18e625a838fe5c99f75
msakibhr <msakibhr@gmail.com>
2023-05-09 13:02:42 +0600

    reacting and reacting!

--- app/javascript/PayTypeSelector/index.jsx
+++ app/javascript/PayTypeSelector/index.jsx
@@ -5,7 +5,7 @@ class PayTypeSelector extends React.Component {
         return (
             <div className="field">
                 <label htmlFor="order_pay_type">Pay type</label>
-                <select id="order_pay_type" name="order[pay_type]">
+                <select onChange={this.onPayTypeSelected} name="order[pay_type]">
                     <option value="">Select a payment method</option>
                     <option value="Check">Check</option>
                     <option value="Credit card">Credit card</option>
