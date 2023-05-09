3da24713abfe1de9051f08379b112e4f8233bf71
msakibhr <msakibhr@gmail.com>
2023-05-09 13:18:56 +0600

    reacting 3

--- app/javascript/PayTypeSelector/index.jsx
+++ app/javascript/PayTypeSelector/index.jsx
@@ -1,21 +1,38 @@
 import React from 'react'
 
+import NoPayType            from './NoPayType';
+import CreditCardPayType    from './CreditCardPayType';
+import CheckPayType         from './CheckPayType';
+import PurchaseOrderPayType from './PurchaseOrderPayType';
+
 class PayTypeSelector extends React.Component {
-    onPayTypeSelected(event) {
-        console.log(event.target.value);
+    constructor(props) {
+        super(props);
+        this.onPayTypeSelected = this.onPayTypeSelected.bind(this);
+        this.state = { selectedPayType: null };
     }
+
+    onPayTypeSelected(event) {
+        this.setState({ selectedPayType: event.target.value });
+    }
+
     render() {
-        return (
-            <div className="field">
-                <label htmlFor="order_pay_type">Pay type</label>
-                <select onChange={this.onPayTypeSelected} name="order[pay_type]">
-                    <option value="">Select a payment method</option>
-                    <option value="Check">Check</option>
-                    <option value="Credit card">Credit card</option>
-                    <option value="Purchase order">Purchase order</option>
-                </select>
-            </div>
-        );
+        let PayTypeCustomComponent = «to be determined»
+return (
+    <div>
+        <div className="field">
+            <label htmlFor="order_pay_type">Pay type</label>
+            <select id="pay_type" onChange={this.onPayTypeSelected}
+                    name="order[pay_type]">
+                <option value="">Select a payment method</option>
+                <option value="Check">Check</option>
+                <option value="Credit card">Credit card</option>
+                <option value="Purchase order">Purchase order</option>
+            </select>
+        </div>
+        <PayTypeCustomComponent />
+    </div>
+);
     }
 }
 export default PayTypeSelector
